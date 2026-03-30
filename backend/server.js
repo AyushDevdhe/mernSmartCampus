ecebcf47e978df6eee6e8234fa8774a8505d55ba
nottlavi <arora.lavi.dev@gmail.com>
2026-03-30 16:04:13 +0530

    remove node_modules and add gitignore

--- backend/server.js
+++ backend/server.js
@@ -1,14 +1,17 @@
 const express = require("express");
+const connectDB = require("./config/database");
+require("dotenv").config();
 
 const app = express();
 
 app.use(express.json());
+connectDB();
 
 app.get("/", (req, res) => {
   res.send("backend app is running");
 });
 
-const PORT = 4000;
+const PORT = process.env.PORT;
 app.listen(PORT, () => {
   console.log(`server running on ${PORT}`);
 });
