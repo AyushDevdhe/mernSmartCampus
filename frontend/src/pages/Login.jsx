e09f6b862f16dce941e4e5c489d5ed22417a53e2
AyushDevdhe <ayushdevdhe64@gmail.com>
2026-04-03 11:00:12 +0530

    Successfully connected Login and Signup with backend. User successfully created and able to Login

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,25 +1,59 @@
-import React from "react";
-import {
-  createBrowserRouter,
-  RouterProvider,
-  createRoutesFromElements,
-  Route,
-} from "react-router-dom";
+import { useState } from "react";
 
+import { loginApi } from "../services/GetService";
 
-function Login(){
-    return (
-      <>
-        {/* <label id="email"></label> */}
-        <input id="email" type="email"></input>
-        <input type="password"></input>
+export const Login = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
 
-        <button type="submit">Login</button>
+  const handleLoginData = async () => {
+    try {
+      const res = await loginApi({ email, password });
+      console.log(res.data);
+      // console.log(res.data.Search || []);
 
-        <h3>Forgot Password</h3>
-        <h4>“don’t have an account? sign up</h4>
-      </>
-    );
-}
+      if (res.data.success) {
+        alert("Login Successful");
+      } else {
+        alert(res.data.message);
+      }
+    } catch (error) {
+      console.log(error);
+      console.log("Error: ", error.message);
+      console.log("Error Status: ", error.response.status);
+      console.log("Error Data: ", error.response.data);
+    }
+  };
+
+  // useEffect(() => {
+  //   getLoginData();
+  // }, []);
+
+  return (
+    <>
+      {/* <label id="email"></label> */}
+
+      <input
+        id="email"
+        type="email"
+        onChange={(e) => setEmail(e.target.value)}
+      ></input>
+      <input
+        type="password"
+        onChange={(e) => setPassword(e.target.value)}
+      ></input>
+
+      <button type="submit" onClick={handleLoginData}>
+        Login
+      </button>
+
+      <h3>Forgot Password</h3>
+      <h4>“don’t have an account? sign up</h4>
+    </>
+  );
+};
 
 export default Login;
+
+// 123456
+// 654321
--- frontend/src/pages/Signup.jsx
+++ frontend/src/pages/Signup.jsx
@@ -1,24 +1,130 @@
-import React from "react";
+import React, { useState } from "react";
 
+import { signupApi } from "../services/GetService";
+import { sendOtpApi } from "../services/GetService";
+export const Signup = () => {
+  const [firstName, registerfirstName] = useState("");
+  const [lastName, registerlastName] = useState("");
+  const [email, registerEmail] = useState("");
+  const [password, registerPassword] = useState("");
+  const [confirmPass, registerConfirmPass] = useState("");
+  const [prn, registerPrn] = useState("");
+  const [otp, registerotp] = useState("");
+  const [sentOtp, registerSentOtp] = useState(false);
 
-function Signup() {
-    return (
-      <>
-        <input type="text" placeholder="First Name"></input>
+  const registerUser = async () => {
+    try {
+      if (password !== confirmPass) {
+        alert("Passwords Do not match");
+        return;
+      }
+      if (!otp) {
+        alert("Enter OTP");
+        return;
+      }
 
-        <input type="text" placeholder="Last Name"></input>
+      const res = await signupApi({
+        firstName,
+        lastName,
+        email,
+        password,
+        prn,
+        otp,
+      });
+      console.log(res.data);
 
-        <input type="email" placeholder="E-Mail"></input>
+      if (res.data.success) {
+        alert("User Created Successfully");
+      } else {
+        alert(res.data.message);
+      }
+    } catch (error) {
+      console.log(error);
+      console.log("Error: ", error.message);
+      console.log("Error Status: ", error.response.status);
+      console.log("Error Data: ", error.response.data);
+    }
+  };
 
-        <input type="password" placeholder="Password"></input>
+  const verify = async () => {
+    try {
+      console.log("Clicked");
 
-        <input type="password" placeholder="Confirm Password"></input>
+      // alert("OTP Sent");
+      const res = await sendOtpApi(email);
+      console.log(res.data);
 
-        <button type="submit">Sign Up</button>
+      if (res.data.success) {
+        alert("OTP Sent");
+        registerSentOtp(true);
+      } else {
+        console.log(res.data.message);
+      }
+    } catch (error) {
+      console.log(error);
+      console.log("Error: ", error.message);
+      console.log("Error Status: ", error.response.status);
+      console.log("Error Data: ", error.response.data);
+    }
+  };
 
-        <h1>already have an account? login</h1>
-      </>
-    );
-}
+  return (
+    <>
+      <input
+        type="text"
+        placeholder="First Name"
+        onChange={(e) => registerfirstName(e.target.value)}
+      ></input>
+
+      <input
+        type="text"
+        placeholder="Last Name"
+        onChange={(e) => registerlastName(e.target.value)}
+      ></input>
+
+      <input
+        type="number"
+        placeholder="PRN"
+        onChange={(e) => registerPrn(e.target.value)}
+      ></input>
+
+      <input
+        type="email"
+        placeholder="E-Mail"
+        onChange={(e) => registerEmail(e.target.value)}
+      ></input>
+
+      <button onClick={verify}>Send Otp</button>
+
+      {sentOtp && (
+        <input
+          type="number"
+          placeholder="Enter OTP"
+          onChange={(e) => registerotp(e.target.value)}
+        />
+      )}
+
+      <input
+        type="password"
+        placeholder="Password"
+        onChange={(e) => registerPassword(e.target.value)}
+      ></input>
+
+      <input
+        type="password"
+        placeholder="Confirm Password"
+        onChange={(e) => registerConfirmPass(e.target.value)}
+      ></input>
+
+      <button type="submit" onClick={registerUser}>
+        Sign Up
+      </button>
+
+      <h1>already have an account? login</h1>
+    </>
+  );
+};
 
 export default Signup;
+
+// Signup button blovked -> otp field -> otp verfivcation -> matched with backend -> signup button unblock
